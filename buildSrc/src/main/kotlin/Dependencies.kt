609aa6b0828608fd5a94790bd55c9bb76b80d64a
Jeongyoon <dlwjddbs14@naver.com>
2022-07-07 20:21:08 +0900

    chore: build src settings

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,13 @@
+object Dependencies {
+    const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
+    const val KOTLIN_JDK = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+
+    const val SPRING_STARTER = "org.springframework.boot:spring-boot-starter-web"
+    const val SPRING_VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
+    const val SPRING_SECURITY = "org.springframework.boot:spring-boot-starter-security"
+
+    const val JACKSON = "com.fasterxml.jackson.module:jackson-module-kotlin"
+
+    const val JPA = "org.springframework.boot:spring-boot-starter-data-jpa"
+    const val MYSQL_CONNECTOR = "mysql:mysql-connector-java"
+}
