94169cd3b642bb216b9146b293772c353a9bcc11
Jeongyoon <dlwjddbs14@naver.com>
2022-07-08 00:43:31 +0900

    feat: entity

---
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/persistence/entity/CommentEntity.kt
@@ -0,0 +1,4 @@
+package com.lt2.lt2hexagonalspringserver.domain.comment.persistence.entity
+
+class CommentEntity {
+}
---
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/presentation/entity/UserEntity.kt
@@ -0,0 +1,5 @@
+package domain.user.presentation.entity
+
+@Entity
+class UserEntity {
+}
