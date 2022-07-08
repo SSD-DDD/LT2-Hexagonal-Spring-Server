3169e62e16af70183d854ebff4def4b92656be10
Jeongyoon <dlwjddbs14@naver.com>
2022-07-08 11:20:20 +0900

    feat: usecase annotation component scan

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/annotation/config/GlobalComponentScan.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/annotation/config/GlobalComponentScan.kt
@@ -1,4 +1,19 @@
 package com.lt2.lt2hexagonalspringserver.global.annotation.config
 
+import annotation.UseCase
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
+
+@Configuration
+@ComponentScan(
+    basePackages = ["com.lt2.lt2hexagonalspringserver"],
+    includeFilters = [
+        ComponentScan.Filter(
+            type = FilterType.ANNOTATION,
+            value = [UseCase::class]
+        )
+    ]
+)
 class GlobalComponentScan {
 }
