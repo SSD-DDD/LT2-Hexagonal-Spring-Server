3d7fe5cc3f08118d56b475a85d3cd4153b271ac5
Jeongyoon <dlwjddbs14@naver.com>
2022-07-08 11:19:29 +0900

    feat: adapter annotation

---
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/annotation/Adapter.kt
@@ -0,0 +1,8 @@
+package com.lt2.lt2hexagonalspringserver.global.annotation
+
+import org.springframework.stereotype.Component
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Component
+annotation class Adapter()
