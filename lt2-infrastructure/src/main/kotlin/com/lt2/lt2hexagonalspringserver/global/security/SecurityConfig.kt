b8f5f40153a948f1edb62ee748d88127bbb259a0
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 01:13:11 +0900

    feat: security config

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/SecurityConfig.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/SecurityConfig.kt
@@ -1,4 +1,31 @@
 package com.lt2.lt2hexagonalspringserver.global.security
 
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
 class SecurityConfig {
+
+    @Bean
+    fun securityFilterConfig(httpSecurity: HttpSecurity): SecurityFilterChain {
+        httpSecurity
+            .csrf().disable()
+            .cors()
+
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("*").permitAll()
+
+        return httpSecurity.build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
 }
