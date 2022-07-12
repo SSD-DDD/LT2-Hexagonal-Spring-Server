29b5b7ffd48095a2929e0e6d314c44cf9bbefd58
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 01:13:37 +0900

    feat: user mapper

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/mapper/UserMapper.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/mapper/UserMapper.kt
@@ -1,4 +1,10 @@
 package com.lt2.lt2hexagonalspringserver.domain.user.mapper
 
+import com.lt2.lt2hexagonalspringserver.domain.user.domain.UserEntity
+import user.User
+
 interface UserMapper {
+    fun userDomainToEntity(user: User): UserEntity
+
+    fun userEntityToDomain(userEntity: UserEntity): User
 }
--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/mapper/UserMapperImpl.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/mapper/UserMapperImpl.kt
@@ -1,4 +1,27 @@
 package com.lt2.lt2hexagonalspringserver.domain.user.mapper
 
-class UserMapperImpl {
+import com.lt2.lt2hexagonalspringserver.domain.user.domain.UserEntity
+import org.springframework.stereotype.Component
+import user.User
+
+@Component
+class UserMapperImpl: UserMapper {
+
+    override fun userDomainToEntity(user: User) =
+        UserEntity(
+            id = user.id,
+            accountId = user.accountId,
+            password = user.password,
+            name = user.name,
+            money = user.money
+        )
+
+    override fun userEntityToDomain(userEntity: UserEntity) =
+        User(
+            id = userEntity.id,
+            accountId = userEntity.accountId,
+            password = userEntity.password,
+            name = userEntity.name,
+            money = userEntity.money
+        )
 }
