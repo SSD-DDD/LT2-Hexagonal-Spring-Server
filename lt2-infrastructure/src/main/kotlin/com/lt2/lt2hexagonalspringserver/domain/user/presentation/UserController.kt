60f5a25b8c0961da96b7455735010eb340863fbb
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 01:14:05 +0900

    feat: user controller

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/presentation/UserController.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/presentation/UserController.kt
@@ -1,4 +1,20 @@
 package com.lt2.lt2hexagonalspringserver.domain.user.presentation
 
-class UserController {
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import user.api.CreateUserApi
+import user.api.dto.request.CreateUserDomainRequest
+
+@RequestMapping("/users")
+@RestController
+class UserController(
+    private val createUserApi: CreateUserApi
+) {
+
+    @PostMapping
+    fun userSignUp(@RequestBody request: CreateUserDomainRequest) {
+        createUserApi.saveUser(request)
+    }
 }
