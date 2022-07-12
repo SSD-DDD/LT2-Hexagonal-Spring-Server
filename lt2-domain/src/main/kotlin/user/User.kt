6d0df542b227f9d6a760a99b0ca0684c6517e9f9
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 01:13:00 +0900

    feat: user domain

--- lt2-domain/src/main/kotlin/user/User.kt
+++ lt2-domain/src/main/kotlin/user/User.kt
@@ -1,4 +1,18 @@
 package user
 
-class User {
-}
+import annotation.Aggregate
+import java.util.UUID
+
+@Aggregate
+class User(
+
+    val id: UUID,
+
+    val accountId: String,
+
+    val password: String,
+
+    val name: String,
+
+    val money: Long
+)
