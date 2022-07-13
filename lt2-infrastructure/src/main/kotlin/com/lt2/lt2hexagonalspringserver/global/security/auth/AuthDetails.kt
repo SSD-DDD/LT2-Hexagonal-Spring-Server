f9bb583fab55e6792bde780475de696495ff0e29
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 13:26:16 +0900

    feat: auth details setting

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/auth/AuthDetails.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/auth/AuthDetails.kt
@@ -1,4 +1,24 @@
 package com.lt2.lt2hexagonalspringserver.global.security.auth
 
-class AuthDetails {
+import com.lt2.lt2hexagonalspringserver.domain.user.domain.UserEntity
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val userEntity: UserEntity
+): UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = userEntity.accountId
+
+    override fun isAccountNonExpired() = true
+
+    override fun isAccountNonLocked() = true
+
+    override fun isCredentialsNonExpired() = true
+
+    override fun isEnabled() = true
 }
