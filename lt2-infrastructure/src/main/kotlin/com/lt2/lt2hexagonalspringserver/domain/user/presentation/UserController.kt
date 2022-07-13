39e5f49986952540221de544dec60ad88e7fe68f
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 11:04:10 +0900

    feat: response status

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/presentation/UserController.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/user/presentation/UserController.kt
@@ -1,8 +1,10 @@
 package com.lt2.lt2hexagonalspringserver.domain.user.presentation
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import user.api.CreateUserApi
 import user.api.dto.request.CreateUserDomainRequest
@@ -13,6 +15,7 @@ class UserController(
     private val createUserApi: CreateUserApi
 ) {
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun userSignUp(@RequestBody request: CreateUserDomainRequest) {
         createUserApi.saveUser(request)
