e86e9f8d08f45905e1d35c5bd9121e9e8fe466dc
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 10:40:26 +0900

    feat: user already exists exception

--- lt2-domain/src/main/kotlin/user/exception/UserAlreadyExistsException.kt
+++ lt2-domain/src/main/kotlin/user/exception/UserAlreadyExistsException.kt
@@ -1,4 +1,11 @@
 package user.exception
 
-object UserAlreadyExistsException {
+import error.code.ErrorMessage
+import error.code.ErrorStatus
+import error.Lt2Exception
+
+object UserAlreadyExistsException: Lt2Exception(
+    ErrorStatus.USER_ALREADY_EXISTS,
+    ErrorMessage.USER_ALREADY_EXISTS
+) {
 }
