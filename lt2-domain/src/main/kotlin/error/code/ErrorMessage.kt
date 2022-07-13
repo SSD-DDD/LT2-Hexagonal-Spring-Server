82cc9e55e034db221de0c57ce1d568b1941dcc02
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 10:37:26 +0900

    feat: error code settings

--- lt2-domain/src/main/kotlin/error/code/ErrorMessage.kt
+++ lt2-domain/src/main/kotlin/error/code/ErrorMessage.kt
@@ -1,6 +1,7 @@
-package error
+package error.code
 
 object ErrorMessage {
     const val USER_ALREADY_EXISTS = "User Already Exists"
     const val USER_NOT_FOUND = "User Not Found"
+    const val INTERNAL_SERVER = "Internal Server"
 }
--- lt2-domain/src/main/kotlin/error/code/ErrorStatus.kt
+++ lt2-domain/src/main/kotlin/error/code/ErrorStatus.kt
@@ -1,6 +1,7 @@
-package error
+package error.code
 
 object ErrorStatus {
-    const val USER_ALREADY_EXISTS = 401
     const val USER_NOT_FOUND = 404
+    const val USER_ALREADY_EXISTS = 409
+    const val INTERNAL_SERVER = 500
 }
