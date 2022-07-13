4e3cf94b341d92862fe93f8e491ce1bfb6bb25c6
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 17:16:46 +0900

    feat: refresh token domain

--- lt2-domain/src/main/kotlin/auth/RefreshToken.kt
+++ lt2-domain/src/main/kotlin/auth/RefreshToken.kt
@@ -1,4 +1,13 @@
 package auth
 
-class RefreshToken {
-}
+import annotation.Aggregate
+
+@Aggregate
+class RefreshToken(
+
+    val accountId: String,
+
+    val token: String,
+
+    val ttl: Long
+)
--- lt2-domain/src/main/kotlin/user/api/dto/response/TokenDomainResponse.kt
+++ lt2-domain/src/main/kotlin/user/api/dto/response/TokenDomainResponse.kt
@@ -4,4 +4,5 @@ data class TokenDomainResponse(
 
     val accessToken: String,
 
+    val refreshToken: String
 )
