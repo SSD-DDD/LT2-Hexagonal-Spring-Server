7f72f4386491339ca77f95b8290e01edb72df0e6
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 17:15:16 +0900

    feat: refresh token mapper

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/auth/mapper/RefreshTokenMapperImpl.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/auth/mapper/RefreshTokenMapperImpl.kt
@@ -1,4 +1,23 @@
 package com.lt2.lt2hexagonalspringserver.domain.auth.mapper
 
-class RefreshTokenMapperImpl {
+import auth.RefreshToken
+import com.lt2.lt2hexagonalspringserver.domain.auth.domain.RefreshTokenEntity
+import org.springframework.stereotype.Component
+
+@Component
+class RefreshTokenMapperImpl: RefreshTokenMapper {
+
+    override fun refreshTokenDomainToEntity(refreshToken: RefreshToken) =
+        RefreshTokenEntity(
+            accountId = refreshToken.accountId,
+            token = refreshToken.token,
+            ttl = refreshToken.ttl
+        )
+
+    override fun refreshTokenEntityToDomain(refreshTokenEntity: RefreshTokenEntity) =
+        RefreshToken(
+            accountId = refreshTokenEntity.accountId,
+            token = refreshTokenEntity.token,
+            ttl = refreshTokenEntity.ttl
+        )
 }
