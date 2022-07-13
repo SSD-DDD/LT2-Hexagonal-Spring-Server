9fec315ee5f47e963ba83e17684ba0462fdfa477
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 10:38:13 +0900

    feat: exception handler

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/error/GlobalExceptionHandler.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/error/GlobalExceptionHandler.kt
@@ -1,4 +1,22 @@
 package com.lt2.lt2hexagonalspringserver.global.error
 
-class GlobalErrorHandler {
+import error.Lt2Exception
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(Lt2Exception::class)
+    fun errorHandle(e: Lt2Exception): ResponseEntity<ErrorResponse> {
+        val errorStatus = e.status
+        val errorMessage = e.message
+
+        return ResponseEntity(
+            ErrorResponse(errorStatus, errorMessage),
+            HttpStatus.valueOf(errorStatus)
+        )
+    }
 }
