e01bde0a12f15539f14b51be128e389d3c57f222
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 13:25:42 +0900

    feat: jwt setting

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/JwtTokenParser.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/JwtTokenParser.kt
@@ -1,4 +1,36 @@
 package com.lt2.lt2hexagonalspringserver.global.security.jwt
 
-class JwtTokenParser {
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.JwtProperties.Companion.PREFIX
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.exception.InvalidJwtException
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.exception.JwtExpiredException
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.exception.JwtValidateException
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.MalformedJwtException
+import org.springframework.stereotype.Component
+
+@Component
+class JwtTokenParser(
+    private val jwtProperties: JwtProperties
+) {
+
+    fun parseToken(token: String): String {
+        if (token.startsWith(PREFIX)) return token.replace(PREFIX, "")
+
+        throw JwtValidateException
+    }
+
+    private fun getTokenBody(token: String): Claims {
+        return try {
+            Jwts.parser().setSigningKey(jwtProperties.secretKey)
+                .parseClaimsJws(token).body
+        } catch (e: Exception) {
+            when (e) {
+                is ExpiredJwtException -> throw JwtExpiredException
+                is MalformedJwtException -> throw JwtValidateException
+                else -> throw InvalidJwtException
+            }
+        }
+    }
 }
--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/JwtTokenProvider.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/JwtTokenProvider.kt
@@ -1,4 +1,24 @@
 package com.lt2.lt2hexagonalspringserver.global.security.jwt
 
-class JwtProvider {
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.JwtProperties.Companion.ACCESS
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class JwtTokenProvider(
+    private val jwtProperties: JwtProperties
+) {
+
+    fun generateAccessToken(accountId: String) = generateToken(accountId, jwtProperties.accessExp, ACCESS)
+
+    private fun generateToken(accountId: String, exp: Long, type: String): String =
+        Jwts.builder()
+            .signWith(SignatureAlgorithm.HS256, jwtProperties.secretKey)
+            .setSubject(accountId)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + exp * 1000))
+            .claim("type", type)
+            .compact()
 }
