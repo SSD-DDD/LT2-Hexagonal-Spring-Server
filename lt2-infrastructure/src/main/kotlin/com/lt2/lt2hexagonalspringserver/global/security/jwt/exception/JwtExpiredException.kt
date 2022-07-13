cf2123651175937440661d04049e64a5fcde802c
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 13:25:55 +0900

    feat: jwt exception

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/exception/JwtExpiredException.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/exception/JwtExpiredException.kt
@@ -1,4 +1,11 @@
 package com.lt2.lt2hexagonalspringserver.global.security.jwt.exception
 
-object JwtExpired {
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.exception.message.JwtErrorMessage.JWT_EXPIRED
+import error.Lt2Exception
+import error.code.GlobalErrorStatus.UNAUTHORIZED_401
+
+object JwtExpiredException: Lt2Exception(
+    UNAUTHORIZED_401,
+    JWT_EXPIRED
+) {
 }
