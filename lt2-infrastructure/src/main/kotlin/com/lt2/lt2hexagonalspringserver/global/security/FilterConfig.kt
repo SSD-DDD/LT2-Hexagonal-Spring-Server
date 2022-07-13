8670ea5b4b66ddf05289fdfe6fd51efb303658b8
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 20:21:34 +0900

    style: package move

---
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/FilterConfig.kt
@@ -0,0 +1,19 @@
+package com.lt2.lt2hexagonalspringserver.global.filter
+
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.JwtTokenParser
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+class FilterConfig(
+    private val jwtTokenParser: JwtTokenParser
+): SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(builder: HttpSecurity) {
+        val jwtTokenFilter = JwtTokenFilter(jwtTokenParser)
+        val exceptionFilter = ExceptionFilter()
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(exceptionFilter, JwtTokenFilter::class.java)
+    }
+}
