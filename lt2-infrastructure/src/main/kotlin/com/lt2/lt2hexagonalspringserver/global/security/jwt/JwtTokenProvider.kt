d0a7b52325af2b454189cc015b78590544527dc9
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 17:16:36 +0900

    feat: refresh token save

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/JwtTokenProvider.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/security/jwt/JwtTokenProvider.kt
@@ -1,6 +1,10 @@
 package com.lt2.lt2hexagonalspringserver.global.security.jwt
 
+import auth.RefreshToken
+import com.lt2.lt2hexagonalspringserver.domain.auth.domain.repository.RefreshTokenPersistenceAdapter
+import com.lt2.lt2hexagonalspringserver.domain.auth.domain.repository.RefreshTokenRepository
 import com.lt2.lt2hexagonalspringserver.global.security.jwt.JwtProperties.Companion.ACCESS
+import com.lt2.lt2hexagonalspringserver.global.security.jwt.JwtProperties.Companion.REFRESH
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.stereotype.Component
@@ -8,10 +12,25 @@ import java.util.*
 
 @Component
 class JwtTokenProvider(
-    private val jwtProperties: JwtProperties
+    private val jwtProperties: JwtProperties,
+    private val refreshTokenPersistenceAdapter: RefreshTokenPersistenceAdapter
 ) {
 
     fun generateAccessToken(accountId: String) = generateToken(accountId, jwtProperties.accessExp, ACCESS)
+
+    fun generateRefreshToken(accountId: String): String {
+        val refreshToken =  generateToken(accountId, jwtProperties.refreshExp, REFRESH)
+
+        refreshTokenPersistenceAdapter.saveRefreshToken(
+            RefreshToken(
+                accountId = accountId,
+                token = refreshToken,
+                ttl = jwtProperties.refreshExp
+            )
+        )
+
+        return refreshToken
+    }
 
     private fun generateToken(accountId: String, exp: Long, type: String): String =
         Jwts.builder()
