aa5d748a5a5a94a218b766928dad76daab37b310
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-13 13:27:12 +0900

    feat: exception

--- lt2-domain/src/main/kotlin/user/exception/UserAlreadyExistsException.kt
+++ lt2-domain/src/main/kotlin/user/exception/UserAlreadyExistsException.kt
@@ -1,11 +1,11 @@
 package user.exception
 
 import error.Lt2Exception
-import error.code.ErrorMessage.USER_NOT_FOUND
-import error.code.ErrorStatus.CONFLICT_409
+import error.code.GlobalErrorStatus.CONFLICT_409
+import user.exception.message.UserErrorMessage.USER_ALREADY_EXISTS
 
 object UserAlreadyExistsException: Lt2Exception(
     CONFLICT_409,
-    USER_NOT_FOUND
+    USER_ALREADY_EXISTS
 ) {
 }
--- lt2-domain/src/main/kotlin/user/exception/UserNotFoundException.kt
+++ lt2-domain/src/main/kotlin/user/exception/UserNotFoundException.kt
@@ -1,8 +1,8 @@
 package user.exception
 
 import error.Lt2Exception
-import error.code.ErrorMessage.USER_NOT_FOUND
-import error.code.ErrorStatus.NOT_FOUND_404
+import error.code.GlobalErrorStatus.NOT_FOUND_404
+import user.exception.message.UserErrorMessage.USER_NOT_FOUND
 
 object UserNotFoundException: Lt2Exception(
     NOT_FOUND_404,
