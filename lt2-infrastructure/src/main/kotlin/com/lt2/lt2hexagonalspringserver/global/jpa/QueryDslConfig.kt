597ca4da5eaa2d8d81a79df518ed4bf98c342577
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-15 01:15:38 +0900

    feat: querydsl setting

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/jpa/QueryDslConfig.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/global/jpa/QueryDslConfig.kt
@@ -1,4 +1,15 @@
 package com.lt2.lt2hexagonalspringserver.global.jpa
 
-class QueryDslConfig {
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+
+@Configuration
+class QueryDslConfig(
+    private val entityManager: EntityManager
+) {
+
+    @Bean
+    fun jpaFactory() = JPAQueryFactory(entityManager)
 }
