4810750d2420d078a87dcf2499fc49243e9640f8
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-16 23:27:35 +0900

    feat: @Transactional

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/feed/domain/repository/FeedPersistenceAdapter.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/feed/domain/repository/FeedPersistenceAdapter.kt
@@ -3,9 +3,11 @@ package com.lt2.lt2hexagonalspringserver.domain.feed.domain.repository
 import com.lt2.lt2hexagonalspringserver.domain.feed.mapper.FeedMapper
 import com.lt2.lt2hexagonalspringserver.global.annotation.Adapter
 import com.lt2.lt2hexagonalspringserver.feed.Feed
+import com.lt2.lt2hexagonalspringserver.feed.api.dto.respons.FeedResponse
 import com.lt2.lt2hexagonalspringserver.feed.exception.FeedNotFoundException
 import com.lt2.lt2hexagonalspringserver.feed.spi.FeedSpi
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
 
 @Adapter
@@ -14,6 +16,7 @@ class FeedPersistenceAdapter(
     private val feedMapper: FeedMapper
 ): FeedSpi {
 
+    @Transactional
     override fun saveFeed(feed: Feed) {
         val feedEntity = feedMapper.feedDomainToEntity(feed)
         feedRepository.save(feedEntity)
@@ -23,6 +26,9 @@ class FeedPersistenceAdapter(
         val feedEntity = jpaFeedById(feedId)
         return feedMapper.feedEntityToDomain(feedEntity)
     }
+
+    @Transactional(readOnly = true)
+    override fun findAll(page: Int): List<FeedResponse> = feedRepository.findAll(page)
 
     private fun jpaFeedById(id: UUID) =
         feedRepository.findByIdOrNull(id) ?: throw FeedNotFoundException
