5687e7de06de304d2fbf75c6c3401e28c4a01c5a
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-16 23:27:56 +0900

    feat: feed list router

--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/feed/presentation/FeedController.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/feed/presentation/FeedController.kt
@@ -2,11 +2,15 @@ package com.lt2.lt2hexagonalspringserver.domain.feed.presentation
 
 import com.lt2.lt2hexagonalspringserver.domain.feed.presentation.dto.request.CreateFeedWebRequest
 import com.lt2.lt2hexagonalspringserver.feed.api.CreateFeedApi
+import com.lt2.lt2hexagonalspringserver.feed.api.QueryFeedListApi
 import com.lt2.lt2hexagonalspringserver.feed.api.dto.request.CreateFeedDomainRequest
+import com.lt2.lt2hexagonalspringserver.feed.spi.QueryFeedSpi
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
@@ -14,7 +18,8 @@ import javax.validation.Valid
 @RequestMapping("/feeds")
 @RestController
 class FeedController(
-    private val createFeedApi: CreateFeedApi
+    private val createFeedApi: CreateFeedApi,
+    private val queryFeedListApi: QueryFeedListApi
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,4 +31,7 @@ class FeedController(
         )
         createFeedApi.execute(feedDomain)
     }
+
+    @GetMapping("/list")
+    fun queryFeedList(@RequestParam page: Int) = queryFeedListApi.execute(page)
 }
