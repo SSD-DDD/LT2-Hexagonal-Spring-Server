9d9fcf01a9315803571771a9986c4238a29dd2a4
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-17 23:41:20 +0900

    test: create feed api

---
+++ lt2-domain/src/test/kotlin/com/lt2/lt2hexagonalspringserver/domain/feed/CreateFeedApiTest.kt
@@ -0,0 +1,26 @@
+package com.lt2.lt2hexagonalspringserver.domain.feed
+
+import com.lt2.lt2hexagonalspringserver.feed.api.CreateFeedApi
+import com.lt2.lt2hexagonalspringserver.feed.api.dto.request.CreateFeedDomainRequest
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+
+class CreateFeedApiTest {
+
+    private val request = mock<CreateFeedDomainRequest>()
+
+    private val createFeedApi = mock<CreateFeedApi>()
+
+    @Test
+    fun execute() {
+
+        given(request.title)
+            .willReturn("제목")
+
+        given(request.content)
+            .willReturn("내용")
+
+        createFeedApi.execute(request)
+    }
+}
