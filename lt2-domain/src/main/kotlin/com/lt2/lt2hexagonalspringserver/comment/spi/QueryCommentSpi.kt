37aad6fe99671891caa3aa16bbd23a6cb9b91676
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-18 11:49:05 +0900

    feat: comment query

--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/spi/QueryCommentSpi.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/spi/QueryCommentSpi.kt
@@ -1,7 +1,9 @@
 package com.lt2.lt2hexagonalspringserver.comment.spi
 
 import com.lt2.lt2hexagonalspringserver.annotation.Spi
+import com.lt2.lt2hexagonalspringserver.comment.Comment
 
 @Spi
 interface QueryCommentSpi {
+    fun saveComment(comment: Comment)
 }
--- lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/domain/repository/CommentPersistenceAdapter.kt
+++ lt2-infrastructure/src/main/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/domain/repository/CommentPersistenceAdapter.kt
@@ -1,12 +1,18 @@
 package com.lt2.lt2hexagonalspringserver.domain.comment.domain.repository
 
+import com.lt2.lt2hexagonalspringserver.comment.Comment
 import com.lt2.lt2hexagonalspringserver.comment.spi.CommentSpi
+import com.lt2.lt2hexagonalspringserver.domain.comment.mapper.CommentMapper
 import com.lt2.lt2hexagonalspringserver.global.annotation.Adapter
 
 @Adapter
 class CommentPersistenceAdapter(
-    private val commentRepository: CommentRepository
+    private val commentRepository: CommentRepository,
+    private val commentMapper: CommentMapper
 ): CommentSpi {
 
-
+    override fun saveComment(comment: Comment) {
+        val commentEntity = commentMapper.commentDomainToEntity(comment)
+        commentRepository.save(commentEntity)
+    }
 }
