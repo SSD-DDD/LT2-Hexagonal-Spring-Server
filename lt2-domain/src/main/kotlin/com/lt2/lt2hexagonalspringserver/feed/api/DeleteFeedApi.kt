d0813fe60be708bdf2c1ad4aa8b253f3f32c1779
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-18 09:30:47 +0900

    feat: delete feed api

--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/feed/api/DeleteFeedApi.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/feed/api/DeleteFeedApi.kt
@@ -1,4 +1,7 @@
 package com.lt2.lt2hexagonalspringserver.feed.api
 
+import java.util.UUID
+
 interface DeleteFeedApi {
+    fun execute(feedId: UUID)
 }
--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/feed/service/DeleteFeedApiImpl.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/feed/service/DeleteFeedApiImpl.kt
@@ -1,4 +1,14 @@
 package com.lt2.lt2hexagonalspringserver.feed.service
 
-class DeleteFeedApiImpl {
+import com.lt2.lt2hexagonalspringserver.annotation.DomainService
+import com.lt2.lt2hexagonalspringserver.feed.api.DeleteFeedApi
+import com.lt2.lt2hexagonalspringserver.feed.spi.QueryFeedSpi
+import java.util.*
+
+@DomainService
+class DeleteFeedApiImpl(
+    private val queryFeedSpi: QueryFeedSpi
+): DeleteFeedApi {
+
+    override fun execute(feedId: UUID) = queryFeedSpi.deleteFeed(feedId)
 }
