ffd304d05ffb0df001a8665502998a45dad286f9
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-18 11:48:29 +0900

    feat: create comment api

--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/api/CreateCommentApi.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/api/CreateCommentApi.kt
@@ -1,4 +1,8 @@
 package com.lt2.lt2hexagonalspringserver.comment.api
 
+import com.lt2.lt2hexagonalspringserver.comment.api.dto.request.CreateCommentDomainRequest
+import java.util.*
+
 interface CreateCommentApi {
+    fun execute(feedId: UUID, request: CreateCommentDomainRequest)
 }
--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/service/CreateCommentApiImpl.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/service/CreateCommentApiImpl.kt
@@ -1,4 +1,27 @@
 package com.lt2.lt2hexagonalspringserver.comment.service
 
-class CreateCommentApiImpl {
+import com.lt2.lt2hexagonalspringserver.annotation.DomainService
+import com.lt2.lt2hexagonalspringserver.comment.Comment
+import com.lt2.lt2hexagonalspringserver.comment.api.CreateCommentApi
+import com.lt2.lt2hexagonalspringserver.comment.api.dto.request.CreateCommentDomainRequest
+import com.lt2.lt2hexagonalspringserver.comment.spi.QueryCommentSpi
+import com.lt2.lt2hexagonalspringserver.user.spi.CurrentUserSpi
+import java.util.*
+
+@DomainService
+class CreateCommentApiImpl(
+    private val queryCommentSpi: QueryCommentSpi,
+    private val currentUserSpi: CurrentUserSpi
+): CreateCommentApi {
+
+    override fun execute(feedId: UUID, request: CreateCommentDomainRequest) {
+        val comment = Comment(
+            commentId = UUID(0, 0),
+            content = request.content,
+            feedId = feedId,
+            userId = currentUserSpi.currentUser().id
+        )
+
+        queryCommentSpi.saveComment(comment)
+    }
 }
