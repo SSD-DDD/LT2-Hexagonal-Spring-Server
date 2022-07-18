6cfd93a86686f90cd01bd9c351a4326327fb0616
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-18 16:55:59 +0900

    feat: update comment

---
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/api/UpdateCommentApi.kt
@@ -0,0 +1,8 @@
+package com.lt2.lt2hexagonalspringserver.comment.api
+
+import com.lt2.lt2hexagonalspringserver.comment.api.dto.request.UpdateCommentDomainRequest
+import java.util.UUID
+
+interface UpdateCommentApi {
+    fun execute(feedId: UUID, request: UpdateCommentDomainRequest)
+}
---
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/api/dto/request/UpdateCommentDomainRequest.kt
@@ -0,0 +1,6 @@
+package com.lt2.lt2hexagonalspringserver.comment.api.dto.request
+
+data class UpdateCommentDomainRequest(
+
+    val content: String
+)
