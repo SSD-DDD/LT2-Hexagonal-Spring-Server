2f17f5add1193dccce4242417e0d0a306bff803d
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-18 11:55:24 +0900

    test: create comment api test

--- lt2-domain/src/test/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/CreateCommentApiTest.kt
+++ lt2-domain/src/test/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/CreateCommentApiTest.kt
@@ -1,4 +1,29 @@
 package com.lt2.lt2hexagonalspringserver.domain.comment
 
+import com.lt2.lt2hexagonalspringserver.comment.api.CreateCommentApi
+import com.lt2.lt2hexagonalspringserver.comment.api.dto.request.CreateCommentDomainRequest
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
 class CreateCommentApiTest {
+
+    private val request = mock<CreateCommentDomainRequest>()
+
+    private val createCommentApi = mock<CreateCommentApi>()
+
+    @Test
+    fun execute() {
+
+        val feedId = UUID(0, 0)
+
+        given(request.content)
+            .willReturn("댓글")
+
+        given(feedId)
+            .willReturn(UUID.randomUUID())
+
+        createCommentApi.execute(feedId, request)
+    }
 }
