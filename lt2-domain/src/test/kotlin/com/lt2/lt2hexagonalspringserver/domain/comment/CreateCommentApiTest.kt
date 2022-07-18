ea8b2b219bad31cfa05c33f5057a0b1a668c4e97
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-18 16:55:07 +0900

    test: create comment api

--- lt2-domain/src/test/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/CreateCommentApiTest.kt
+++ lt2-domain/src/test/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/CreateCommentApiTest.kt
@@ -2,12 +2,9 @@ package com.lt2.lt2hexagonalspringserver.domain.comment
 
 import com.lt2.lt2hexagonalspringserver.comment.api.CreateCommentApi
 import com.lt2.lt2hexagonalspringserver.comment.api.dto.request.CreateCommentDomainRequest
-import com.lt2.lt2hexagonalspringserver.feed.exception.FeedNotFoundException
+import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.mock
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.BDDMockito.given
-import org.mockito.BDDMockito.`when`
 import java.util.UUID
 
 class CreateCommentApiTest {
