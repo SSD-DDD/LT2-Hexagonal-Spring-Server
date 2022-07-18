d665bcda9a6b25d33110da551c356c6b8dfca6af
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-18 13:48:10 +0900

    test: create comment api

--- lt2-domain/src/test/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/CreateCommentApiTest.kt
+++ lt2-domain/src/test/kotlin/com/lt2/lt2hexagonalspringserver/domain/comment/CreateCommentApiTest.kt
@@ -2,9 +2,12 @@ package com.lt2.lt2hexagonalspringserver.domain.comment
 
 import com.lt2.lt2hexagonalspringserver.comment.api.CreateCommentApi
 import com.lt2.lt2hexagonalspringserver.comment.api.dto.request.CreateCommentDomainRequest
-import com.nhaarman.mockitokotlin2.given
+import com.lt2.lt2hexagonalspringserver.feed.exception.FeedNotFoundException
 import com.nhaarman.mockitokotlin2.mock
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.`when`
 import java.util.UUID
 
 class CreateCommentApiTest {
@@ -13,17 +16,15 @@ class CreateCommentApiTest {
 
     private val createCommentApi = mock<CreateCommentApi>()
 
+    private val uuid = mock<UUID>()
+
     @Test
     fun execute() {
-
-        val feedId = UUID(0, 0)
 
         given(request.content)
             .willReturn("댓글")
 
-        given(feedId)
-            .willReturn(UUID.randomUUID())
-
-        createCommentApi.execute(feedId, request)
+        createCommentApi.execute(uuid, request)
     }
+
 }
