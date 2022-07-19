ca4ddfe751d7499bea2f95b85eea390b24dafd2f
Jeong Yoon <dlwjddbs14@naver.com>
2022-07-19 13:39:38 +0900

    feat: comment error

--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/exception/CommentNotFoundException.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/exception/CommentNotFoundException.kt
@@ -1,4 +1,11 @@
 package com.lt2.lt2hexagonalspringserver.comment.exception
 
-class CommentNotFoundException {
+import com.lt2.lt2hexagonalspringserver.comment.exception.message.CommentErrorMessage.COMMENT_NOT_FOUND
+import com.lt2.lt2hexagonalspringserver.error.Lt2Exception
+import com.lt2.lt2hexagonalspringserver.error.code.GlobalErrorStatus.NOT_FOUND_404
+
+object CommentNotFoundException: Lt2Exception(
+    NOT_FOUND_404,
+    COMMENT_NOT_FOUND
+) {
 }
--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/exception/message/CommentErrorMessage.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/comment/exception/message/CommentErrorMessage.kt
@@ -1,4 +1,5 @@
 package com.lt2.lt2hexagonalspringserver.comment.exception.message
 
 object CommentErrorMessage {
+    const val COMMENT_NOT_FOUND = "Comment Not Found"
 }
