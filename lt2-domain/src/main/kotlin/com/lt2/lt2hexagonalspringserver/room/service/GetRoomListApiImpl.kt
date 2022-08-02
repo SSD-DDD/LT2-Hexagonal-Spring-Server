97b72fd3b3290aa65c18f390d3970e5dd33873ed
Jeong Yoon <dlwjddbs14@naver.com>
2022-08-03 01:04:13 +0900

    feat: room list response

--- lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/room/service/GetRoomListApiImpl.kt
+++ lt2-domain/src/main/kotlin/com/lt2/lt2hexagonalspringserver/room/service/GetRoomListApiImpl.kt
@@ -1,4 +1,11 @@
 package com.lt2.lt2hexagonalspringserver.room.service
 
-class GetRoomListApiImpl {
+import com.lt2.lt2hexagonalspringserver.room.api.GetRoomListApi
+import com.lt2.lt2hexagonalspringserver.room.api.dto.response.RoomListResponse
+
+class GetRoomListApiImpl: GetRoomListApi {
+
+    override fun execute(): RoomListResponse {
+        TODO("Not yet implemented")
+    }
 }
